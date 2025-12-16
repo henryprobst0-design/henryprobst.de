ccd45be8eea4a88cc268d42a7cc95e2d9184a49b
henryprobst0-design <henryprobst0@gmail.com>
2025-12-16 15:51:26 +0100

    Create contact.vue

---
+++ pages/contact.vue
@@ -0,0 +1,28 @@
+<script setup>
+definePageMeta({
+  layout: "main",
+});
+useHead({
+  title: 'Kontakt',
+})
+</script>
+<template>
+<section class="content">
+  <div class="container mx-auto">
+    <div class="prose">
+      <h1 class="text-center">Kontaktiere mich!</h1>
+
+      <div class="cards max-w-[300px] mx-auto">
+        <div class="contact | flex items-center">
+          <div class="i-mdi-gmail w-6 h-6 bg-red-500"></div>
+          <a href="mail@henryprobst.de">mail@henryprobst.de</a>
+        </div>
+        <div class="contact | flex items-center">
+          <div class="i-mdi-linkedin w-6 h-6 bg-blue-500"></div>
+          <a href="https://linkedin.com/in/henry-probst-1b8420389" target="_blank">Henry Probst</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+</template>
