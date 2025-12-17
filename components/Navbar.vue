8ae24e29b432e3dd0f71db269a654417ebfae806
henryprobst0-design <henryprobst0@gmail.com>
2025-12-17 18:22:35 +0100

    Update Navbar.vue

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -16,87 +16,60 @@ const isDark = useDark({
   valueDark: 'dark',
   valueLight: 'light',
 })
-
 const toggleDark = useToggle(isDark)
 </script>
 
 <template>
   <nav>
-    <!-- BRAND -->
-    <div class="navbar__brand italic text-center flex justify-center py-3 px-8">
+    <div class="navbar__brand | italic text-center flex justify-center py-3 px-8">
       <div class="w-1/3"></div>
-
       <h3 class="w-1/3 text-xl my-0 flex items-center justify-center">
-        <router-link
-          to="/"
-          class="decoration-none dark:text-white!"
-        >
-          Henry Probst
-        </router-link>
+        <router-link to="/" class="decoration-none | dark:text-white!">Henry Probst</router-link>
       </h3>
-
       <div class="w-1/3 flex justify-end gap-3 items-center">
-        <!-- DARK MODE TOGGLE (MOBILE) -->
+        <!-- Dark Toggle Mobile -->
         <a
           href="#"
-          class="navbar__link p-2! md:hidden block"
+          class="navbar__link p-2 md:hidden block"
           @click.prevent="toggleDark()"
         >
-          <div
-            class="i-mdi-theme-light-dark text-xl
-                   text-gray-900 dark:text-gray-100
-                   hover:text-gray-600 dark:hover:text-gray-300"
-          ></div>
+          <div class="i-mdi-theme-light-dark text-xl text-black dark:text-white"></div>
         </a>
 
-        <!-- MENU TOGGLE (MOBILE) -->
+        <!-- Menu Button -->
         <button
-          class="navbar__link p-2! md:hidden block"
+          class="p-2 bg-transparent border-none md:hidden inline-block"
           type="button"
           aria-label="Toggle Menu"
           @click="isNavbarOpen = !isNavbarOpen"
         >
-          <div
-            class="i-mdi-menu text-xl
-                   text-gray-900 dark:text-gray-100
-                   hover:text-gray-600 dark:hover:text-gray-300"
-          ></div>
+          <div class="i-mdi-menu text-xl text-black dark:text-white"></div>
         </button>
       </div>
     </div>
 
-    <!-- MENU -->
     <div class="navbar__menu">
       <ul
+        class="flex decoration-none list-none gap-5 justify-center p-0 flex-col md:flex-row text-right pr-8 lg:p-0 lg:text-center"
         v-show="isNavbarOpen"
-        class="flex list-none gap-5 justify-center p-0
-               flex-col md:flex-row text-right pr-8
-               lg:p-0 lg:text-center"
       >
         <li class="navbar__item">
           <nuxt-link to="/" class="navbar__link">Über mich</nuxt-link>
         </li>
-
         <li class="navbar__item">
           <nuxt-link to="/erfolge" class="navbar__link">Erfolge</nuxt-link>
         </li>
-
         <li class="navbar__item">
           <nuxt-link to="/kontakt" class="navbar__link">Kontakt</nuxt-link>
         </li>
-
-        <!-- DARK MODE TOGGLE (DESKTOP) -->
         <li class="navbar__item hidden md:block">
+          <!-- Dark Toggle Desktop -->
           <a
             href="#"
             class="navbar__link p-0!"
             @click.prevent="toggleDark()"
           >
-            <div
-              class="i-mdi-theme-light-dark text-xl
-                     text-gray-900 dark:text-gray-100
-                     hover:text-gray-600 dark:hover:text-gray-300"
-            ></div>
+            <div class="i-mdi-theme-light-dark text-xl text-black dark:text-white"></div>
           </a>
         </li>
       </ul>
